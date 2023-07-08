afcc43e93b92f90d68699c06d2e1a1e8c1829be2
vichet vatt <vichetvattedu@gmail.com>
2023-07-08 14:55:04 +0700

    Added image and icon

--- Rich App/ViewController.swift
+++ Rich App/ViewController.swift
@@ -8,9 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let view = UIView()
+        view.backgroundColor = .black
         // Do any additional setup after loading the view.
     }
 
